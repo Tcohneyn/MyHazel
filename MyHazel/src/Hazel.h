9014d0cd406c43d78d4c91bfa46679b9ff17f4ce
Tcohneyn <chenminami@outlook.com>
2025-05-31 05:07:51 +0800

    006-日志记录 添加了spdlog库

--- MyHazel/src/Hazel.h
+++ MyHazel/src/Hazel.h
@@ -2,6 +2,7 @@
 //For use by Hazel applications
 #include <stdio.h>
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 //-------Entry Ponit--------
 #include "Hazel/EntryPoint.h"
 //--------------------------
